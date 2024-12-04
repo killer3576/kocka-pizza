2b4fd5bd37609460fc8353742afe9e3bdfa99c3d
killer3576 <adalberto.merenda@gmail.com>
2024-12-04 09:57:45 +0100

    Add files via upload

---
+++ kocka.js
@@ -0,0 +1,75 @@
+const app = Vue.createApp({
+  data: function () {
+    return {
+      // variables
+      random: 0,
+    };
+  },
+  methods: {
+    // functions
+    getRanNum: function () {
+      var min = 1;
+      var max = 7;
+      this.random = Math.floor(Math.random() * (max - min) + min);
+    },
+  },
+  computed: {
+    // "auto" functions/variables
+    getFillA: function () {
+      if (this.random === 1 || this.random === 3 || this.random === 5) {
+        return "black";
+      } else {
+        return "white";
+      }
+    },
+
+    getFillB: function () {
+      if (this.random === 1) {
+        return "white";
+      } else {
+        return "black";
+      }
+    },
+
+    getFillC: function () {
+      if (this.random === 6) {
+        return "black";
+      } else {
+        return "white";
+      }
+    },
+
+    getFillD: function () {
+      if (this.random === 4 || this.random === 5 || this.random === 6) {
+        return "black";
+      } else {
+        return "white";
+      }
+    },
+    getFillE: function () {
+      if (this.random === 4 || this.random === 5 || this.random === 6) {
+        return "black";
+      } else {
+        return "white";
+      }
+    },
+    getFillF: function () {
+      if (this.random === 6) {
+        return "black";
+      } else {
+        return "white";
+      }
+    },
+    getFillG: function () {
+      if (this.random != 1) {
+        return "black";
+      } else {
+        return "white";
+      }
+    },
+  },
+  mounted: function () {},
+});
+
+app.use(Quasar);
+app.mount("#app");
